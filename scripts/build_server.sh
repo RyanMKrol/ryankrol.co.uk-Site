de68891e0245ff08497b4d5e345d5d07c3e49f2e
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-15 16:54:17 +0000

    Export NVM explicitly from CodeDeploy scripts

--- scripts/build_server.sh
+++ scripts/build_server.sh
@@ -3,6 +3,7 @@
 # PATH isn't loaded when doing stuff via CodeDeploy, so you need to load
 # it at each step of the process for npm to actually work.
 source /home/ec2-user/.bash_profile
+export NVM_DIR="/home/ec2-user/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
 npm install
 npm run build
