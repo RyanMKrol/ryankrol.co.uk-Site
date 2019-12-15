d68d919c9f32fc49c6b1159e3c0ee9750bcd99f6
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-15 18:00:12 +0000

    Add pm2 save and logging to CodeDeploy scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,9 +6,13 @@ source /home/ec2-user/.bash_profile
 export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
+echo "Going into the right directory."
 cd /home/ec2-user/ryankrol.co.uk
+echo "About to start the app."
 pm2 start lib/app.js
 echo "Finished starting the app."
+pm2 save
+echo "Saved the state of pm2."
 cd /home/ec2-user
 echo "Deleting the repository."
 rm -rf ryankrol.co.uk
