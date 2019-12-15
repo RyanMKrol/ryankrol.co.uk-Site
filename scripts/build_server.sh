43852a02051dcd703418df8223bd52039196dd24
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-15 16:51:50 +0000

    Load PATH in build scripts

--- scripts/build_server.sh
+++ scripts/build_server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# PATH isn't loaded when doing stuff via CodeDeploy, so you need to load
+# it at each step of the process for npm to actually work.
+source /home/ec2-user/.bash_profile
+
 npm install
 npm run build
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# PATH isn't loaded when doing stuff via CodeDeploy, so you need to load
+# it at each step of the process for npm to actually work.
+source /home/ec2-user/.bash_profile
+
 npm run start
