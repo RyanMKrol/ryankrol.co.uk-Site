ab54795c8527dfbab72be99b85d8ee6275a8796f
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-17 19:56:03 +0000

    Update AppRouter to use header

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -7,20 +7,26 @@ import {
 
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Header from "./components/Header"
+
+import './AppRouter.css';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div id="root-container">
+      <Router>
+        <Header />
+        <div className="page-body">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
   )
 }
