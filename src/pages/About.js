6f1fbeebc6e1dc58b55e3792be864840cd6a6524
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-17 19:55:48 +0000

    Update pages to move header out

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import {
-  Link
-} from "react-router-dom"
+
 import logo from './../assets/logo.svg';
+
 import './About.css';
 
 function About() {
   return (
-    <div className="About">
-      <header className="About-header">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
+    <div className="container">
+      <div className="About">
         <img src={logo} className="About-logo" alt="logo" />
         <p>
           About Page - Under Development!
         </p>
-      </header>
+      </div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import {
-  Link
-} from "react-router-dom"
+
 import logo from './../assets/logo.svg';
+
 import './Home.css';
 
 function Home() {
   return (
     <div className="Home">
-      <header className="Home-header">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-        <img src={logo} className="Home-logo" alt="logo" />
-        <p>
-          Home Page - Under Development!
-        </p>
-      </header>
+      <img src={logo} className="Home-logo" alt="logo" />
+      <p>
+        Home Page - Under Development!
+      </p>
     </div>
   );
 }
