11fd4c82cc887689f011020e0272d62fd52adfb9
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-17 18:24:25 +0000

    Organise testing files

--- src/tests/AppRouter.test.js
+++ src/tests/AppRouter.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 
-import AppRouter from './AppRouter';
+import AppRouter from './../AppRouter';
 
 it('renders welcome message', () => {
     const appRouter = renderer
