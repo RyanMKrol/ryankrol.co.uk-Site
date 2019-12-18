32fe07a047023d4641f3c059fb97cfd70b21fde2
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-18 01:53:26 +0000

    Update tests for header and app router

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -2,9 +2,49 @@ import React from 'react'
 import { shallow, mount } from 'enzyme'
 import toJson from 'enzyme-to-json'
 
+import {
+  BrowserRouter as Router,
+} from "react-router-dom"
 import Header from './../Header'
 
-it('renders router only when rendered', () => {
-    const header = shallow(<Header />)
+it('renders header correctly with no currentPage being passed through', () => {
+    const header = shallow(
+      <Header.WrappedComponent
+        location={{}}
+      />)
     expect(toJson(header)).toMatchSnapshot()
+})
+
+it('creates the correct current page links with generateHeaderLinks, passing through / as current page', () => {
+    const headerLinks = new Header.WrappedComponent().generateHeaderLinks({
+      location: {
+        pathname: "/"
+      }
+    })
+
+    expect(headerLinks).toMatchSnapshot()
+})
+
+it('creates the correct current page links with generateHeaderLinks, passing through /about as current page', () => {
+    const headerLinks = new Header.WrappedComponent().generateHeaderLinks({
+      location: {
+        pathname: "/about"
+      }
+    })
+
+    expect(headerLinks).toMatchSnapshot()
+})
+
+it('creates the correct current page links with generateHeaderLinks, passing through an unsupported destination current page', () => {
+    const headerLinks = new Header.WrappedComponent().generateHeaderLinks({
+      location: {
+        pathname: "/unsupported"
+      }
+    })
+
+    expect(headerLinks).toMatchSnapshot()
+})
+
+it('renders header without throwing', () => {
+    expect(() => mount(<Router><Header /></Router>)).not.toThrow()
 })
