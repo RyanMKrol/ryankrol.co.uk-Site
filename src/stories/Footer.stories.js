8cea269cf41a1d8616701bbd00f3213abe16f55e
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-19 03:10:15 +0000

    Add Footer entry to Storybook

---
+++ src/stories/Footer.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from './../components/Footer'
+
+export default {
+  title: 'Footer',
+};
+
+export const normal = () => (
+  <div style={{"background-color": "black"}}>
+    <Footer />
+  </div>
+)
