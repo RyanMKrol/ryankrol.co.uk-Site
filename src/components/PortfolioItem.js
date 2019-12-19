9431cd5d3c55488fd85ad7b49530a084afb0d2aa
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-19 20:20:51 +0000

    Add PortfolioItem component

---
+++ src/components/PortfolioItem.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import './PortfolioItem.css';
+
+class PortfolioItem extends Component {
+  render() {
+    return (
+      <div className="PortfolioItem">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dui vivamus arcu felis bibendum ut tristique et egestas. Sed nisi lacus sed viverra tellus. Morbi tincidunt ornare massa eget egestas. Pharetra magna ac placerat vestibulum. Et molestie ac feugiat sed lectus vestibulum mattis. Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique. Non pulvinar neque laoreet suspendisse interdum consectetur. Tempus quam pellentesque nec nam aliquam sem et. Nisl nisi scelerisque eu ultrices vitae auctor eu augue. Sed faucibus turpis in eu. Habitant morbi tristique senectus et netus et malesuada fames ac.
+      </p>
+      </div>
+    )
+  }
+}
+
+export default PortfolioItem
