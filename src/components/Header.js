98bd63bd6e951fea146c2faf9aaf0ff42dd94963
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-19 03:20:13 +0000

    Remove redundant page styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,7 +38,7 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-      <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet" />
         <div className="header-content">
           <div className="social-container">
             <SocialList />
