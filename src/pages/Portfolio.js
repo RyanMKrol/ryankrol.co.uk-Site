d5a96fa2421d82c75a630a5b1f25c3f7d42ee67d
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-19 20:21:13 +0000

    Add Portfolio page, and export all pages

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import logo from './../assets/logo.svg';
+
+import './Portfolio.css';
+
+function Portfolio() {
+  return (
+    <div className="container">
+      <div className="Portfolio">
+        <img src={logo} className="Portfolio-logo" alt="logo" />
+        <p>
+          Portfolio Page - Under Development!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
