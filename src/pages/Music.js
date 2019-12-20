bfe6172dda3f41c1dab98aee27194857cf5eb8df
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-20 14:00:18 +0000

    Add Music page content
    * A loading spinner displays
    * Data is fetched from API
    * Need to use data to display UI

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, {
+  Component
+} from 'react'
+import fetch from "node-fetch"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 
 import logo from './../assets/logo.svg';
 
 import './Music.css';
 
-function Music() {
-  return (
-    <div className="container">
-      <div className="Music">
-        <img src={logo} className="Music-logo" alt="logo" />
-        <p>
-          Music Page - Under Development!
-        </p>
+const ALBUM_API_ENDPOINT = "http://www.ryankrol.co.uk/api/albums"
+
+class Music extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      albums: undefined
+    }
+
+    const albumData = fetch(ALBUM_API_ENDPOINT).then((response) => {
+      return response.json()
+    }).then((albumData) => {
+      this.setState({
+        albums: albumData
+      })
+    })
+  }
+
+  render() {
+    console.log(this.state)
+    const albumsLoaded = false
+
+    const content = albumsLoaded ?
+      <p>Albums have loaded</p> :
+      <FontAwesomeIcon
+        className ="fa-spin"
+        icon={faCircleNotch}
+        size="6x"
+      />
+
+    return (
+      <div className="music-page-body">
+        <div className="Music">
+          {content}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 
 export default Music;
