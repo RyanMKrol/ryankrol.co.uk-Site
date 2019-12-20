6b960e3e6e4aa4fd20029fb282d35ec4963d80e6
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-20 12:17:25 +0000

    Move Header data out to it's own file
    * I'm not sure where the data for this app should live
    just yet, so I'm going to compartmentalise it for now
    and revisit later.

---
+++ src/data/HeaderData.js
@@ -0,0 +1,18 @@
+const HEADER_ITEMS = [
+  {
+    destinationPage: "/",
+    content: "Home",
+  },
+  {
+    destinationPage: "/about",
+    content: "About",
+  },
+  {
+    destinationPage: "/portfolio",
+    content: "Portfolio",
+  },
+]
+
+export {
+  HEADER_ITEMS
+}
