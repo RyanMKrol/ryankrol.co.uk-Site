0fd6750de661627418741c3b325154fdb14b7663
Ryan Krol <ryankr@hotmail.co.uk>
2020-01-18 23:32:50 +0000

    Add BookItem storybook entry

---
+++ src/stories/BookItem.stories.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import BookItem from './../components/BookItem'
+
+export default {
+  title: 'BookItem',
+};
+
+export const normal = () => (
+  <div style={{display: "flex"}}>
+    <BookItem
+      title={"12 Rules for Life"}
+      authors={["Jordan B. Peterson"]}
+      images={{
+        "smallThumbnail": "http://books.google.com/books/content?id=u8w_DwAAQBAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+        "thumbnail": "http://books.google.com/books/content?id=u8w_DwAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+      }}
+      isbn={"9780241351659"}
+      bookId={"u8w_DwAAQBAJ"}
+    />
+  </div>
+)
