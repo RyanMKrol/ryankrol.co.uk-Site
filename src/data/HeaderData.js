db452568643a6c1b28be7c0ece34609a5d4d2469
Ryan Krol <ryankr@hotmail.co.uk>
2020-01-18 21:36:22 +0000

    Add Books page
    * The Books page is a straight copy of Music for now
    * AppRouter links added
    * Books added to header

--- src/data/HeaderData.js
+++ src/data/HeaderData.js
@@ -15,6 +15,10 @@ const HEADER_ITEMS = [
     destinationPage: "/music",
     content: "Music",
   },
+  {
+    destinationPage: "/books",
+    content: "Books",
+  },
 ]
 
 export { HEADER_ITEMS }
---
+++ src/pages/Books.js
@@ -0,0 +1,65 @@
+import React, {
+  Component
+} from 'react'
+import fetch from "node-fetch"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
+import { AlbumItem } from './../components'
+
+import './Books.css';
+
+const ALBUM_API_ENDPOINT = "http://www.ryankrol.co.uk/api/albums"
+
+class Books extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      albums: undefined
+    }
+
+    fetch(ALBUM_API_ENDPOINT).then((response) => {
+      return response.json()
+    }).then((albumData) => {
+      this.setState({
+        albums: albumData
+      })
+    })
+  }
+
+  generateAlbumLinks() {
+    return this.state.albums.map((album) => (
+      <AlbumItem
+        key={album.albumLink}
+        artist={album.artist}
+        albumName={album.albumName}
+        thumbnail={album.thumbnail}
+        albumLink={album.albumLink}
+        playcount={album.playcount}
+      />
+    ))
+  }
+
+  render() {
+    const albumsLoaded = this.state.albums !== undefined
+    const content = albumsLoaded ?
+      this.generateAlbumLinks() :
+      <FontAwesomeIcon
+        className ="fa-spin"
+        icon={faCircleNotch}
+        size="6x"
+      />
+
+    return (
+      <div className="books-page-body">
+        <div className="Books">
+          {content}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Books;
