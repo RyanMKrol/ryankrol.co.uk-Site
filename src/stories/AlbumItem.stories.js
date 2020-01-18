a160dc184902290f87ff3edfe0c16a3ef17df2bc
Ryan Krol <ryankr@hotmail.co.uk>
2020-01-18 23:32:10 +0000

    Add BookItem storybook entry, and fix AlbumItem entry

--- src/stories/AlbumItem.stories.js
+++ src/stories/AlbumItem.stories.js
@@ -12,6 +12,7 @@ export const normal = () => (
       albumName={"Ten$ion"}
       thumbnail={"https://lastfm.freetls.fastly.net/i/u/300x300/d3561c0bd8c01a27bc3e8991a68f45f4.png"}
       albumLink={"/"}
+      playcount={123}
     />
   </div>
 )
