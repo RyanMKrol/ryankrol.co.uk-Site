af66249e44bac634c0da24d0a5baec16a0787e90
RyanMKrol <ryankrol.m@gmail.com>
2021-04-02 21:19:35 +0100

    Fix eslint errors on Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react';
 
 import './Footer.css';
 
-class Footer extends Component {
-  render() {
-    return (
-      <div className="Footer" />
-    )
-  }
+/**
+ * Class representing a UI component for the footer
+ *
+ * @returns {React.Component} The JSX representing the component
+ */
+function Footer() {
+  return <div className="Footer" />;
 }
 
-export default Footer
+export default Footer;
