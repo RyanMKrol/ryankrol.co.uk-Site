43ae4eaaac8e6dbcd1dd1b2813e6bd7d146a8118
RyanMKrol <ryankrol.m@gmail.com>
2021-04-02 21:29:59 +0100

    Fix eslint errors for loading icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css';
 /**
  * Class representing a UI component for the footer
  *
- * @returns {React.Component} The JSX representing the component
+ * @returns {string} The JSX representing the component
  */
 function Footer() {
   return <div className="Footer" />;
--- src/components/LoadingIcon.js
+++ src/components/LoadingIcon.js
@@ -1,17 +1,14 @@
-import React, {Component} from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
-class LoadingIcon extends Component {
-  render() {
-    return (
-      <FontAwesomeIcon
-        className ="fa-spin"
-        icon={faCircleNotch}
-        size="6x"
-      />
-    );
-  }
+/**
+ * Class representing a UI component for the loading icon
+ *
+ * @returns {string} HTML markup for the loading icon
+ */
+function LoadingIcon() {
+  return <FontAwesomeIcon className="fa-spin" icon={faCircleNotch} size="6x" />;
 }
 
 export default LoadingIcon;
