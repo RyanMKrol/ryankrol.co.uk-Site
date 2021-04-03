609572e985e1da7d366fd505115a9f8bc2a0fc70
RyanMKrol <ryankrol.m@gmail.com>
2021-04-03 02:28:26 +0100

    Fix lint errors for utils files

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,9 +1,13 @@
+/**
+ * Returns the endpoint to use for API calls
+ *
+ * @returns {string} Base API endpoint to use
+ */
 function baseEndpoint() {
   if (process.env.REACT_APP_LOCAL_API !== undefined) {
-    return 'http://localhost:8000/api'
-  } else {
-    return 'http://www.ryankrol.co.uk/api'
+    return 'http://localhost:8000/api';
   }
+  return 'http://www.ryankrol.co.uk/api';
 }
 
-export default baseEndpoint
+export default baseEndpoint;
--- src/utils/ratingsFormUtils.js
+++ src/utils/ratingsFormUtils.js
@@ -1,40 +1,75 @@
-import { Component } from 'react'
-import { Field, ErrorMessage } from 'formik'
+import { Component } from 'react';
+import { Field, ErrorMessage } from 'formik';
 
-const RANGE_TYPE = 'range'
-const PASSWORD_TYPE = 'password'
+const RANGE_TYPE = 'range';
+const PASSWORD_TYPE = 'password';
 
+/**
+ * Class used to display errors on form validation
+ */
 class CustomError extends Component {
+  /**
+   * @param {Component} args Children to render in error
+   */
   constructor(args) {
-    super()
+    super();
 
-    this.children = args.children
+    this.children = args.children;
   }
 
+  /**
+   * renders the widget
+   *
+   * @returns {string} The HTML for the validation error
+   */
   render() {
-    return <div className="validation-error">{this.children}</div>
+    return <div className="validation-error">{this.children}</div>;
   }
 }
 
+/**
+ * Generates the title of the field
+ *
+ * @param {string} fieldTitle The title of the field
+ * @param {string} fieldType The type of field we're rendering
+ * @param {number} currentValue The current value of the field (only used for ranges)
+ * @returns {string} Title to use for the current field
+ */
 function generateFieldTitle(fieldTitle, fieldType, currentValue) {
   if (fieldType === RANGE_TYPE) {
-    return `${fieldTitle} - ${currentValue}`
-  } else {
-    return `${fieldTitle}`
+    return `${fieldTitle} - ${currentValue}`;
   }
+  return `${fieldTitle}`;
 }
 
+/**
+ * Generates the tag to use to fetch input
+ *
+ * @param {string} fieldName fieldName
+ * @param {string} fieldTitle fieldTitle
+ * @param {string} fieldType fieldType
+ * @returns {string} HTML representing just the field
+ */
 function generateFieldTag(fieldName, fieldTitle, fieldType) {
   switch (fieldType) {
     case PASSWORD_TYPE:
-      return <Field type={PASSWORD_TYPE} name={fieldName} autoComplete="current-password" />
+      return <Field type={PASSWORD_TYPE} name={fieldName} autoComplete="current-password" />;
     case RANGE_TYPE:
-      return <Field type={RANGE_TYPE} name={fieldName} min="0" max="100" />
+      return <Field type={RANGE_TYPE} name={fieldName} min="0" max="100" />;
     default:
-      return <Field type={fieldType} name={fieldName} autoComplete="off" />
+      return <Field type={fieldType} name={fieldName} autoComplete="off" />;
   }
 }
 
+/**
+ * Generates a wrapped input for gathering data
+ *
+ * @param {string} fieldName fieldName
+ * @param {string} fieldTitle fieldTitle
+ * @param {string} fieldType fieldType
+ * @param {number} currentValue currentValue
+ * @returns {string} HTML representing the field and associated wrappers
+ */
 function generateField(fieldName, fieldTitle, fieldType, currentValue) {
   return (
     <div key={fieldName} className="form-item">
@@ -43,7 +78,7 @@ function generateField(fieldName, fieldTitle, fieldType, currentValue) {
       {generateFieldTag(fieldName, fieldTitle, fieldType)}
       <ErrorMessage name={fieldName} component={CustomError} />
     </div>
-  )
+  );
 }
 
-export { RANGE_TYPE, PASSWORD_TYPE, generateField }
+export { RANGE_TYPE, PASSWORD_TYPE, generateField };
