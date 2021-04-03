3b95226dd70fed89afe5fe78a2a1d8fcc4bb0894
RyanMKrol <ryankrol.m@gmail.com>
2021-04-03 02:15:38 +0100

    Fix lint errors for Books page

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,36 +1,58 @@
-import React, { Component } from 'react'
-import fetch from 'node-fetch'
+import React, { Component } from 'react';
+import fetch from 'node-fetch';
 
-import { BookItem, LoadingIcon } from './../components'
-import { BOOKS_PAGE_DATA } from './../data'
-import { apiUtils } from './../utils'
+import { BookItem, LoadingIcon } from '../components';
+import { BOOKS_PAGE_DATA } from '../data';
+import { apiUtils } from '../utils';
 
-import './Books.css'
+import './Books.css';
 
-const BOOKS_API_ENDPOINT = `${apiUtils.default()}/books`
+const BOOKS_API_ENDPOINT = `${apiUtils.default()}/books`;
 
+/**
+ * Object representing the props to an AlbumItem
+ *
+ * @typedef {object} RawBookJsonData
+ * @property {string} bookId BookId
+ * @property {string} title Title
+ * @property {string} authors Authors
+ * @property {string} images Images
+ * @property {string} isbn ISBN
+ * @property {string} series The series that the book belongs to
+ * @property {number} numberInSeries The number in the series that this book belongs to
+ */
+
+/**
+ * Page to display my books
+ */
 class Books extends Component {
+  /**
+   * constructor
+   */
   constructor() {
-    super()
+    super();
 
     this.state = {
       books: undefined,
-    }
+    };
 
     fetch(BOOKS_API_ENDPOINT)
-      .then((response) => {
-        return response.json()
-      })
+      .then((response) => response.json())
       .then((booksData) => {
         this.setState({
           books: booksData.sort(bookComparator),
-        })
-      })
+        });
+      });
   }
 
+  /**
+   * Generates book items using books stored in state
+   *
+   * @returns {string} HTML for every book item available to the page
+   */
   generateBookItems() {
     if (typeof this.state.books === 'undefined') {
-      return <LoadingIcon />
+      return <LoadingIcon />;
     }
 
     return this.state.books.map((book) => (
@@ -42,24 +64,34 @@ class Books extends Component {
         isbn={book.isbn}
         bookId={book.bookId}
       />
-    ))
+    ));
   }
 
+  /**
+   * Generates the summary line for the books page
+   *
+   * @returns {string} HTML for the books summary line
+   */
   generatePageSummary() {
     if (typeof this.state.books === 'undefined') {
-      return null
+      return null;
     }
 
     return (
       <div className="books-page-summary">
         <h3>{BOOKS_PAGE_DATA.summary}</h3>
       </div>
-    )
+    );
   }
 
+  /**
+   * Renders the page
+   *
+   * @returns {string} HTML for the page
+   */
   render() {
-    const content = this.generateBookItems()
-    const pageSummary = this.generatePageSummary()
+    const content = this.generateBookItems();
+    const pageSummary = this.generatePageSummary();
 
     return (
       <div className="books-page-body">
@@ -68,18 +100,23 @@ class Books extends Component {
           {content}
         </div>
       </div>
-    )
+    );
   }
 }
 
-// sort books first by the series they belong to, and by the number in
-// that series if two books belong to the same series
+/**
+ * Sorts books first by the series they belong to, and by the number in
+ * that series if two books belong to the same series
+ *
+ * @param {RawBookJsonData} bookA First book in comparison
+ * @param {RawBookJsonData} bookB Second book in comparison
+ * @returns {boolean} Whether bookA should come before bookB
+ */
 function bookComparator(bookA, bookB) {
   if (bookA.series === bookB.series) {
-    return bookA.numberInSeries < bookB.numberInSeries ? -1 : 1
-  } else {
-    return bookA.series > bookB.series ? 1 : -1
+    return bookA.numberInSeries < bookB.numberInSeries ? -1 : 1;
   }
+  return bookA.series > bookB.series ? 1 : -1;
 }
 
-export default Books
+export default Books;
