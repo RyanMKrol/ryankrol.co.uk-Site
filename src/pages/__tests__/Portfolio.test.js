451ba029112b500aedbb8152281ef4aba8d97847
RyanMKrol <ryankrol.m@gmail.com>
2021-04-03 02:21:38 +0100

    Fix lint errors for the page tests

--- src/pages/__tests__/Portfolio.test.js
+++ src/pages/__tests__/Portfolio.test.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { shallow, mount } from 'enzyme'
-import toJson from 'enzyme-to-json'
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
-import Portfolio from './../Portfolio'
+import Portfolio from '../Portfolio';
 
 // Render Tests
 
 it('renders Portfolio only when rendered', () => {
-    const portfolio = shallow(<Portfolio />)
-    expect(toJson(portfolio)).toMatchSnapshot()
-})
+  const portfolio = shallow(<Portfolio />);
+  expect(toJson(portfolio)).toMatchSnapshot();
+});
 
 it('does not crash when rendered', () => {
-    expect(() => mount(<Portfolio />)).not.toThrow()
-})
+  expect(() => mount(<Portfolio />)).not.toThrow();
+});
