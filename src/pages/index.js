e6a57512dd94e771ffc497aa09c33e4852d7a727
RyanMKrol <ryankrol.m@gmail.com>
2021-04-03 02:07:15 +0100

    Fix lint errors in pages index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-export { default as AlbumRatings} from './AlbumRatings'
-export { default as Books } from './Books'
-export { default as MovieRatings } from './MovieRatings'
-export { default as Movies } from './Movies'
-export { default as Music } from './Music'
-export { default as Playground } from './Playground'
-export { default as Portfolio } from './Portfolio'
+export { default as AlbumRatings } from './AlbumRatings';
+export { default as Books } from './Books';
+export { default as MovieRatings } from './MovieRatings';
+export { default as Movies } from './Movies';
+export { default as Music } from './Music';
+export { default as Playground } from './Playground';
+export { default as Portfolio } from './Portfolio';
