15b80e33e2fe38451c71d9434e9be3da8ec269fa
RyanMKrol <ryankrol.m@gmail.com>
2021-06-12 19:48:42 +0100

    Add RatingsGraph component
    * Will let me display ratings information

---
+++ src/components/RatingsGraph/RatingsGraph.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import DEFAULT_MOCK from './__mocks__/RatingsGraphMocks';
+
+export default {
+  title: 'Components/RatingsGraph',
+};
+
+/**
+ * default story
+ *
+ * @returns {React.Component} JSX for the RatingsGraph
+ */
+export const normal = () => DEFAULT_MOCK;
---
+++ src/components/RatingsGraph/__mocks__/RatingsGraphMocks.js
@@ -0,0 +1,10 @@
+import RatingsGraph from '../RatingsGraph';
+
+const DEFAULT_MOCK = (
+  <RatingsGraph
+    data={[10, 20, 30, 40, 50]}
+    labels={['Label1', 'Label2', 'Label3', 'Label4', 'Label5']}
+  />
+);
+
+export default DEFAULT_MOCK;
