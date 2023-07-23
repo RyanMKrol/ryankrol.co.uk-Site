8e87be64e0b5a817ceb28441ff3649c97e70acf6
RyanMKrol <ryankrol.m@gmail.com>
2023-07-23 13:55:27 +0100

    Update prettier style rules

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -4,5 +4,6 @@
  * @param {Response} res response
  */
 export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' });
+  console.log('things');
 }
