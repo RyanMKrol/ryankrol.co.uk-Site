45c7bb4813244c073607bce9d74c34cd5763be1f
RyanMKrol <ryankrol.m@gmail.com>
2023-07-29 23:20:33 +0100

    Add page to display my listening data

---
+++ src/pages/listens/index.js
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+
+import Layout from '@/components/Layout';
+import ListenDisplayCard from '@/components/ListenDisplayCard';
+
+import styles from './index.module.css';
+
+export default function Page({ data }) {
+  return (
+    <Layout>
+      <Head>
+        <title>What I've been listening to!</title>
+      </Head>
+      <div className={styles.cardContainer}>
+        {data.map((item) => (
+          <ListenDisplayCard
+            key={`${item.name}-${item.artist}`}
+            artist={item.artist}
+            name={item.name}
+            thumbnail={item.thumbnail}
+            plays={item.plays}
+          />
+        ))}
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://ryankrol.co.uk/api/listens`).catch((err) => {
+    // decide what to do here when the API call fails
+  });
+
+  const data = await res.json();
+
+  const displayData = data.filter((item) => item.plays > 10);
+
+  // Pass data to the page via props
+  return { props: { data: displayData } };
+}
