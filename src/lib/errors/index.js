7921240fecabfa433a6b450f98790453d15f8ded
RyanMKrol <ryankrol.m@gmail.com>
2023-07-29 23:38:44 +0100

    Remove error classes

--- src/lib/errors/index.js
+++
@@ -1,4 +0,0 @@
-export { default as BookInformationNotFound } from './BookInformationNotFound';
-export { default as RequestNotAuthorised } from './RequestNotAuthorised';
-export { default as ThumbnailNotFound } from './ThumbnailNotFound';
-export { default as PostBodyMissingRequiredData } from './PostBodyMissingRequiredData';
