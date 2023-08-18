3bc8bc513cf66cf20c110ef8874a8bfd4fcd1136
RyanMKrol <ryankrol.m@gmail.com>
2023-08-18 22:15:08 +0100

    Add google analytics integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import '@fontsource/ibm-plex-mono/700.css';
 import '@fontsource/ibm-plex-mono/300.css';
 import '@/styles/globals.css';
@@ -7,6 +9,24 @@ import SiteHeader from '@/components/SiteHeader';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-W68SVNFP1J`}
+      />
+      <Script
+        id="googleAnalytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-W68SVNFP1J', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <SiteHeader />
       <Component {...pageProps} />
     </>
